147480a833912488fc71463a31136ba699727397
m_hirakawa <m_hirakawa@val.co.jp>
2020-12-15 18:52:13 +0900

    関数を切り出した

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const convert = require('xml-js');
 const options = {ignoreComment: true, alwaysChildren: true, compact: true};  // format setting to convert from xml to json
 const dateFormat = require('dateformat');
 const date = new Date();
+const shapeOutput = require('./src/shapeOutput');
 
 const keywords = process.argv[2] ? process.argv[2] : 'GitHub';
 const howManyDays = process.argv[3] ? parseInt(process.argv[3]) : 1;
@@ -22,7 +23,7 @@ axios({
     let news;
     if (Array.isArray(items)){
         if (outputFormat == 'mrkdwn') {
-            news = "<https://codezine.jp/article/detail/13341|「Kubernetes 1.20」がリリース、11の拡張機能が安定版へ、15の拡張機能がベータ版へ移行 - CodeZine（コードジン）>\\n\\n<https://www.jiji.com/jc/article?k=000003599.000005875&g=prt|セキュリティコンテストCTFの花形ジャンルpwnableの解説書！ 『解題pwnable セキュリティコンテストに挑戦しよう！』発行! 技術の泉シリーズ、12月の新刊 - 時事通信>\\n\\n<https://japan.techrepublic.com/article/35163707.htm|「Kubernetes」入門--大規模な展開に最適なコンテナーオーケストレーションシステム - TechRepublic Japan>";
+            news = shapeOutput.toMrkdwn();
         } else {  // dafault: json format
             news = items.map(data => {
                 let item = {};
