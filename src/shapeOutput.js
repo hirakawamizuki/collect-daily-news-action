92eb85d5aef94bd6db7c8f3a41a9fdc6b97a8ae1
m_hirakawa <m_hirakawa@val.co.jp>
2020-12-15 19:19:52 +0900

    コードをきれいにした

--- src/shapeOutput.js
+++ src/shapeOutput.js
@@ -21,6 +21,5 @@ exports.toMrkdwn = (jsonArray) => {
     } else {
         mrkdwn = "";
     }
-    // return "<https://codezine.jp/article/detail/13341|「Kubernetes 1.20」がリリース、11の拡張機能が安定版へ、15の拡張機能がベータ版へ移行 - CodeZine（コードジン）>\\n\\n<https://www.jiji.com/jc/article?k=000003599.000005875&g=prt|セキュリティコンテストCTFの花形ジャンルpwnableの解説書！ 『解題pwnable セキュリティコンテストに挑戦しよう！』発行! 技術の泉シリーズ、12月の新刊 - 時事通信>\\n\\n<https://japan.techrepublic.com/article/35163707.htm|「Kubernetes」入門--大規模な展開に最適なコンテナーオーケストレーションシステム - TechRepublic Japan>";
     return mrkdwn;
 }
