6e5e562d2e60cfec239b5767e192499474da3438
hirakawamizuki <m_hirakawa@val.co.jp>
2020-12-15 22:13:06 +0900

    複数キーワードが設定された時に整形を行うようにした

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const date = new Date();
 const shapeOutput = require('./src/shapeOutput');
 
 // Get inputs from node command arguments
-const keywords = process.argv[2] ? process.argv[2] : 'GitHub';
+const keywords = process.argv[2] ? process.argv[2].split(/, */).map(keyword => keyword.replace(/ /g, '+')) : ['GitHub'];
 const howManyDays = process.argv[3] ? parseInt(process.argv[3]) : 1;
 const outputFormat = process.argv[4] ? process.argv[4] : 'json';
 
 // Creeate google rss url
 const after = `after:${dateFormat(date.setDate(date.getDate() - howManyDays), 'yyyy-mm-dd')}`;  // e.g. after = 'after:2020-12-01'
-const googleRssUrl = `https://news.google.com/rss/search?q=${keywords}+${after}&hl=ja&gl=JP&ceid=JP:ja`;
+const googleRssUrl = `https://news.google.com/rss/search?q=${keywords[0]}+${after}&hl=ja&gl=JP&ceid=JP:ja`;
 
 axios({
     method: 'get',
