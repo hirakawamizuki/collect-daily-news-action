503ebb94d699bbceeb74bca6245af135f796cae2
m_hirakawa <m_hirakawa@val.co.jp>
2020-12-15 17:44:18 +0900

    改行できるかのテスト

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ axios({
     let news;
     if (Array.isArray(items)){
         if (outputFormat == 'mrkdwn') {
-            news = "<https://codezine.jp/article/detail/13341|「Kubernetes 1.20」がリリース、11の拡張機能が安定版へ、15の拡張機能がベータ版へ移行 - CodeZine（コードジン）>'\n'\n<https://www.jiji.com/jc/article?k=000003599.000005875&g=prt|セキュリティコンテストCTFの花形ジャンルpwnableの解説書！ 『解題pwnable セキュリティコンテストに挑戦しよう！』発行! 技術の泉シリーズ、12月の新刊 - 時事通信>'\n'\n<https://japan.techrepublic.com/article/35163707.htm|「Kubernetes」入門--大規模な展開に最適なコンテナーオーケストレーションシステム - TechRepublic Japan>";
+            news = "<https://codezine.jp/article/detail/13341|「Kubernetes 1.20」がリリース、11の拡張機能が安定版へ、15の拡張機能がベータ版へ移行 - CodeZine（コードジン）>\n\n<https://www.jiji.com/jc/article?k=000003599.000005875&g=prt|セキュリティコンテストCTFの花形ジャンルpwnableの解説書！ 『解題pwnable セキュリティコンテストに挑戦しよう！』発行! 技術の泉シリーズ、12月の新刊 - 時事通信>\n\n<https://japan.techrepublic.com/article/35163707.htm|「Kubernetes」入門--大規模な展開に最適なコンテナーオーケストレーションシステム - TechRepublic Japan>";
         } else {  // dafault: json format
             news = items.map(data => {
                 let item = {};
