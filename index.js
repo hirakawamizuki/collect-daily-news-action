1a809863e5cbdd29b69db3293faa219ee784eb0e
m_hirakawa <m_hirakawa@val.co.jp>
2020-12-16 19:16:46 +0900

    複数キーワード検索に対応した

--- index.js
+++ index.js
@@ -10,19 +10,24 @@ const keywords = process.argv[2] ? process.argv[2].split(/,/) : ['GitHub'];
 const howManyDays = process.argv[3] ? parseInt(process.argv[3]) : 1;
 const outputFormat = process.argv[4] ? process.argv[4] : 'json';
 
-// Creeate google rss url
+// Use Google RSS parameter
 const after = `after:${dateFormat(date.setDate(date.getDate() - howManyDays), 'yyyy-mm-dd')}`;  // e.g. after = 'after:2020-12-01'
-const googleRssUrl = encodeURI(`https://news.google.com/rss/search?q=${keywords[0]}+${after}&hl=ja&gl=JP&ceid=JP:ja`);
 
-axios({
-    method: 'get',
-    url: googleRssUrl,
-}).then(res => {
+// Get news data from Google RSS by single keyword
+const getNews = async (keyword, after) => {
+    // Create url for Google RSS 
+    const googleRssUrl = encodeURI(`https://news.google.com/rss/search?q=${keyword}+${after}&hl=ja&gl=JP&ceid=JP:ja`);
+    const res = await axios({ method: 'get', url: googleRssUrl })
     const xml = res.data;
     const json = convert.xml2js(xml, options);
     const items = json.rss.channel.item;
-    const jsonOutput = shapeOutput.toJson(items);
+    return shapeOutput.toJson(items);
+}
 
+// Output news data by using all keywords
+Promise.all(keywords.map(async keyword => await getNews(keyword, after)))
+.then((res) => {
+    const jsonOutput = res.reduce((array1, array2) => array1.concat(array2));
     if (outputFormat == 'mrkdwn') {
         const mrkDwnOutput = shapeOutput.toMrkdwn(jsonOutput);
         console.log(mrkDwnOutput);
@@ -31,4 +36,4 @@ axios({
     }
 }).catch(error => {
     console.log(error);
-})
+});
