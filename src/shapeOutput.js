e67553fe5cf7cccf1ed55fc8f2951398c7cc133c
m_hirakawa <m_hirakawa@val.co.jp>
2020-12-17 18:03:51 +0900

    検索結果が1件だった場合にうまくアウトプットされていなかったバグを解消

--- src/shapeOutput.js
+++ src/shapeOutput.js
@@ -1,25 +1,28 @@
-exports.toJson = (items) => {
-    let json;
-    if (Array.isArray(items)){
-        json = items.map(data => {
+exports.toJson = (news) => {
+    if (Array.isArray(news)){
+        return news.map(data => {
             let item = {};
             item['title'] = data.title._text.replace(/[|()]/g, '');
             item['link'] = data.link._text;
             return item;
         });
+    } else if (news) { // Not array but a single object
+        let item = {};
+        item['title'] = news.title._text.replace(/[|()]/g, '');
+        item['link'] = news.link._text;
+        return item;
     } else {
-        json = [];
+        return [];
     }
-    return json;
 }
 
-exports.toMrkdwn = (jsonArray) => {
-    let mrkdwn;
-    if (Array.isArray(jsonArray)) {
-        const links = jsonArray.map(item => `<${item.link}|${item.title}>`);
-        mrkdwn = links.join('\\n\\n')
+exports.toMrkdwn = (json) => {
+    if (Array.isArray(json)) {
+        const links = json.map(item => `<${item.link}|${item.title}>`);
+        return links.join('\\n\\n')
+    } else if (json) { // Not array but a single object
+        return `<${json.link}|${json.title}>`;
     } else {
-        mrkdwn = "";
+        return "";
     }
-    return mrkdwn;
 }
